40c11d65970763a6fee9921dc78a1b5b3c80767b
dimon19994 <dik19994@gmail.com>
2022-06-27 20:40:18 +0300

    more data

--- main.py
+++ main.py
@@ -13,17 +13,30 @@ callback = []
 def hello_world():
     try:
         show = callback[-1]
+        row = ""
+        if show != "":
+            for i in show.items():
+                row += f"{i[0]}  ->  {i[1]}<br><br>"
     except:
-        show = "NO DATA"
-    return show
+        row = "NO DATA"
+
+    return row
 
 
 @app.route('/add', methods=["POST", "GET"])
 def add():
-   data = request.json if request.is_json else request.data
-   callback.append(data)
 
-   return {"status": "200 OK"}
+    # data = request.json if request.is_json else request.data
+    data = {
+        "json": request.json,
+        "data": request.data,
+        "headers": request.headers,
+        "values": request.values,
+        "method": request.method
+    }
+    callback.append(data)
+
+    return {"status": "200 OK"}
 
 
 if __name__ == '__main__':
